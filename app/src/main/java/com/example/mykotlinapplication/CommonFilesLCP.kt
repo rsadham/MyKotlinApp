86a70f71a24817a11f8e80acaad4b3cb21eaf306
rsadhamnttdata <sadham.hussain@nttdata.com>
2026-01-21 14:17:12 +0530

    code updated.

--- app/src/main/java/com/example/mykotlinapplication/CommonFilesLCP.kt
+++ app/src/main/java/com/example/mykotlinapplication/CommonFilesLCP.kt
@@ -782,6 +782,56 @@ fun invertTree(root: TreeNode?): TreeNode? {
     return root
 }
 
+/* Binary Search
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+https://leetcode.com/problems/binary-search/description/
+*/
+fun binarySearch(nums: IntArray,target:Int):Int{
+    var left = 0
+    var right = nums.size - 1
+    while(left<=right){
+        val mid = left + (right - left) / 2
+
+        when {
+            nums[mid] == target -> return mid
+            nums[mid] < target -> left = mid + 1
+            else -> right = mid - 1
+        }
+
+    }
+    return -1
+}
+
+/* First Bad Version
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+https://leetcode.com/problems/first-bad-version/description/
+*/
+fun isBadVersion(n: Int): Boolean{
+    return true
+}
+fun firstBadVersion(n:Int): Int {
+    var left = 1
+    var right = n
+
+    while (left < right){
+        val mid = left + (right - left) / 2
+
+        if(isBadVersion(mid)){
+            right = mid
+        }else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+
+
+
 
 
 
